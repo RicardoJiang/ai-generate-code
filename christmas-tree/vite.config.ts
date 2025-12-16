16fc484cdca30c4d091ddcd7288457d330b1d305
RicardoJiang <2868405029@qq.com>
2025-12-16 21:10:43 +0800

    MOD: 修改输出为单 html

--- christmas-tree/vite.config.ts
+++ christmas-tree/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/ai-generate-code/',
+  plugins: [react(), viteSingleFile()],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+  },
 })
 
